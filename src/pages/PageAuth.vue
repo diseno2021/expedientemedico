e579eb100d555b9d319d7da0e4b9d6a5ca442e67
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2021-05-02 16:26:59 -0600

    Update PageAuth.vue

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -43,6 +43,7 @@ export default {
                     .catch((error) => {
                         console.error("Error writing document: ", error);
                     });
+                    this.$router.push("/")
 
                 })
                 .catch((err) => {
