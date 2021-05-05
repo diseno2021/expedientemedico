72eeca6f8825a59bb191f2503d504709de8c3ad4
noe martinez <mr18116@ues.edu.sv>
2021-05-05 15:30:29 -0600

    modificacion de los componentes de la vista informacion personal y antecedentes

--- src/pages/ExpedientePaciente/Antecedentes.vue
+++ src/pages/ExpedientePaciente/Antecedentes.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>
-        <div class="row justify-center">
-            <div class="col-md-10">
-                <q-editor v-model="editor" min-height="5rem" />
-                <q-btn>Guardar</q-btn>
-            </div>
-        </div>
+    <div class="q-ma-md">
+        <div class="row">
+      <div class="text-h4">Antecedentes</div>
+      <div class="col-md-12">
+        <q-editor v-model="editor" min-height="5rem" />
+        <q-btn class="q-mt-sm">Guardar</q-btn>
+      </div>
+    </div>
+    <div class="row q-mt-md">
+      <div class="col-md-12">
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
     </div>
 </template>
 
--- src/pages/ExpedientePaciente/InformacionPersonal.vue
+++ src/pages/ExpedientePaciente/InformacionPersonal.vue
@@ -1,67 +1,96 @@
 <template>
-  <div>
-    <div class="row justify-center">
-      <div class="col-md-10">
-        <p class="text">Nombre Completo</p>
-        <q-input outlined />
-        <div class="row justify-between">
-          <div class="col-md-5">
-            <p>Fecha de nacimiento</p>
-            <q-input filled v-model="date" mask="date" :rules="['date']">
-              <template v-slot:append>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy
-                    ref="qDateProxy"
-                    transition-show="scale"
-                    transition-hide="scale"
-                  >
-                    <q-date v-model="date">
-                      <div class="row items-center justify-end">
-                        <q-btn
-                          v-close-popup
-                          label="Close"
-                          color="primary"
-                          flat
-                        />
-                      </div>
-                    </q-date>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-          <div class="col-md-5">
-            <p class="text">Edad</p>
-            <q-input outlined />
-          </div>
-        </div>
-        <div class="row justify-between">
-          <div class="col-md-5">
-            <p>Género</p>
-            <q-radio v-model="genero" val="masculino" label="Masculino" />
-            <q-radio v-model="genero" val="femenino" label="Femenino" />
-          </div>
-          <div class="col-md-5">
-            <p class="text">Tipo de sangre</p>
-            <q-input outlined />
-          </div>
-        </div>
-
-        <div class="col">
-          <p class="text">Dirección</p>
-          <q-input outlined />
-        </div>
-
-        <div class="col">
-          <p class="text">Teléfono</p>
-          <q-input outlined />
-        </div>
-
-        <div class="col">
-          <p class="text">Caso de emergencia</p>
-          <q-input outlined />
-        </div>
+  <div class="q-ma-md">
+    <div class="row q-mt-md">
+      <div class="col-md-12 q-mt-md">
+        <q-input v-model="text" label="Nombre completo">
+          <template v-slot:prepend>
+            <q-icon name="person" />
+          </template>
+        </q-input>
       </div>
+    </div>
+    <div class="row justify-between q-mt-md">
+      <div class="col-md-5">
+        <q-input
+          label="fecha de nacimiento"
+          filled
+          v-model="date"
+          mask="date"
+          :rules="['date']"
+        >
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy
+                ref="qDateProxy"
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-date v-model="date">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+      <div class="col-md-5">
+        <q-input v-model="text" label="Edad"
+          ><template v-slot:prepend>
+            <q-icon name="cake" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+
+    <div class="row justify-between">
+      <div class="col-md-5">
+        <q-radio v-model="genero" val="masculino" label="Masculino" />
+        <q-radio v-model="genero" val="femenino" label="Femenino" />
+      </div>
+      <div class="col-md-5">
+        <q-input v-model="text" label="Tipo de sangre">
+          <template v-slot:prepend>
+            <q-icon name="water" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+
+    <div class="row justify-between">
+      <div class="col-md-4">
+        <q-input v-model="text" label="Peso">
+          <template v-slot:prepend>
+            <q-icon name="person" />
+          </template>
+        </q-input>
+      </div>
+      <div class="col-md-6"></div>
+    </div>
+
+    <div class="col-md-12 q-mt-md">
+      <q-input v-model="text" label="Dirección">
+        <template v-slot:prepend>
+          <q-icon name="directions" />
+        </template>
+      </q-input>
+    </div>
+
+    <div class="col-md-12 q-mt-md">
+      <q-input v-model="text" label="Teléfono">
+        <template v-slot:prepend>
+          <q-icon name="phone" />
+        </template>
+      </q-input>
+    </div>
+
+    <div class="col-md-12 q-mt-md">
+      <q-input v-model="text" label="Caso de emergencia">
+        <template v-slot:prepend>
+          <q-icon name="warning" />
+        </template>
+      </q-input>
     </div>
   </div>
 </template>
@@ -73,7 +102,8 @@ export default {
     return {
       date: "2020/02/01",
       edad: "",
-      genero: ""
+      genero: "",
+      text: ""
     };
   },
   components: {
