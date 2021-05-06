1a8baa9347252374ab54aacf9a35e177bc015314
ernesto-1998 <ernestoalonso1998@gmail.com>
2021-05-06 11:46:49 -0600

    Actualizando expansion item de pasar consulta

--- src/components/PasarConsulta/NavegacionConsulta.vue
+++ src/components/PasarConsulta/NavegacionConsulta.vue
@@ -1,61 +1,92 @@
 <template>
   <div>
     <q-list bordered class="rounded-borders">
-      <q-expansion-item
-        expand-separator
-        label="Antecedentes"
-      >
+      <q-expansion-item expand-separator label="Antecedentes">
         <q-card>
-          <q-card-section> Hola </q-card-section>
+          <q-card-section v-html="paciente.antecedentes"> </q-card-section>
         </q-card>
       </q-expansion-item>
 
       <q-expansion-item expand-separator label="Medicamentos">
         <q-card>
-          <q-card-section> Hola </q-card-section>
+          <q-card-section v-html="paciente.medicamentosPermanentes">
+            
+          </q-card-section>
         </q-card>
       </q-expansion-item>
 
-      <q-expansion-item
-        expand-separator
-        label="Consultas"
-      >
+      <q-expansion-item expand-separator label="Consultas">
         <q-card>
           <q-card-section> Hola </q-card-section>
         </q-card>
       </q-expansion-item>
 
-      <q-expansion-item label="Recetas" expand-separator>
+      <q-expansion-item label="Recetas" expand-separator > 
+        <q-card>
+          <q-card-section v-html="consultas.receta">  </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+      <q-expansion-item label="Archivos" expand-separator>
+        <q-card>
+          <q-card-section> {{paciente.archivos}} </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+      <q-expansion-item label="Peso" expand-separator>
         <q-card>
           <q-card-section> Hola </q-card-section>
         </q-card>
       </q-expansion-item>
-
-       <q-expansion-item label="Archivos" expand-separator>
-        <q-card>
-          <q-card-section> Hola </q-card-section>
-        </q-card>
-      </q-expansion-item>
-
-       <q-expansion-item label="Peso" expand-separator>
-        <q-card>
-          <q-card-section> Hola </q-card-section>
-        </q-card>
-      </q-expansion-item>
-
     </q-list>
   </div>
 </template>
 
 <script>
-
 export default {
   data: () => ({
     paciente: {
-      id:"asdfasdfasdfa987asdfasdf76as",
+      id: "asdfasdfasdfa987asdfasdf76as",
+      idMedico: "asdfasd88asd7fas6dfasd", //este campo relaciona al medico al cual pertenece
       nombre: "Juan Santiago Perez Lima",
-      foto:"https://cdn.quasar.dev/img/avatar6.jpg",
-    }
+      fechaNacimiento: "1998-03-06T00:00:00",
+      genero: "Masculino",
+      peso: [170, 150, 140, 135], //el peso se lleva en un arreglo para poder graficarlo
+      estatura: [123, 125, 135],
+      foto: "http://storage/123231.jpg&quot;",
+      tipoSangre: "O RH+",
+      direccion: "Final avenida fray felipe de jesus moraga casa #23",
+      telefono: "7366-2344",
+      dui: "",
+      whatsapp: "1242-1234",
+      email: "paciente@outlook.com",
+      enCasoEmergencia: "Esposa Juana Salazar 7346454",
+      comentario: "",
+      antecedentes: "Padre diabético<br> Tía materna hipertensa", //texto html
+      alergias: "Penicilina Procainica, picadas de abejas",
+      enfermedadesCronicas: "Cancer de Seno, Diabetes tipo 2", //texto html
+      medicamentosPermanentes: "<br>Rivoflavina</br> una por las noches", //texto html
+      archivos: [
+        "http://storage/idpaciente/foto01.jpg&#39;",
+        "http://storage/idpaciente/foto01.jpg&#39;",
+      ],
+    },
+    consultas: {
+      id: "asdfasdlkjjash9783789asdf",
+      idPaciente: "asdfasdfasdfa987asdfasdf76as",
+      peso: 132.5, //peso en libras
+      estatura: 123, //en centimetros
+      temperatura: 36.5,
+      imc: 23.3,
+      presionArterial: "180/40",
+      exploracionFisica: "", //texto html
+      motivoConsulta: "",
+      sintomas: "", //texto html
+      diagnostico: "", //texto html
+      receta: "<br>2 pastillas de Paracetamol</br> <br>1 tableta de rivotril </br>", //texto html
+      examenes: "", //texto html
+      proximaCita: "2021-04-30T12:21:21",
+    },
   }),
 };
 </script>
