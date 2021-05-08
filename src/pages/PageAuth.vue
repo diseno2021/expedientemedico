7633b9ec0642d5123b09d0440af6dc7f47072589
Edgardo Espadero <em16006@ues.edu.sv>
2021-05-07 18:17:37 -0600

    pantalla principal

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -1,11 +1,80 @@
 <template>
-  <div class="q-pa-md">
+  <div class="login">
     <div class="leftDiv">
-      <p>LOGIN</p>
+      <!-- <div class="top_header_left_side">
+        <q-icon name="medication" size="1rem" />
+        Consultas médicas
+      </div> -->
 
-      <q-btn color="primary" @click="googleAutenticar">
-        Iniciar Sesion con Google
-      </q-btn>
+      <div class="header">
+        <!--Content before waves-->
+        <div class="inner-header flex">
+          <q-icon name="medication" size="2rem" /><h4 style="margin:0">Sistema de consultas</h4>
+        </div>
+
+        <!--Waves Container-->
+        <div>
+          <svg
+            class="waves"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28"
+            preserveAspectRatio="none"
+            shape-rendering="auto"
+          >
+            <defs>
+              <path
+                id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              />
+            </defs>
+            <g class="parallax">
+              <use
+                xlink:href="#gentle-wave"
+                x="48"
+                y="0"
+                fill="rgba(255,255,255,0.7"
+              />
+              <use
+                xlink:href="#gentle-wave"
+                x="48"
+                y="3"
+                fill="rgba(255,255,255,0.5)"
+              />
+              <use
+                xlink:href="#gentle-wave"
+                x="48"
+                y="5"
+                fill="rgba(255,255,255,0.3)"
+              />
+              <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+          </svg>
+        </div>
+        <!--Waves end-->
+      </div>
+      <!--Header ends-->
+
+      <div class="horizontalCenter">
+        <h1 style="color: primary">VirtualDoc</h1>
+        <q-avatar>
+          <img src="/login/logo.jpg" class="logo" />
+        </q-avatar>
+        <br />
+        <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label="Iniciar Sesion con Google"
+          icon="img:/login/google.png"
+          @click="googleAutenticar"
+        />
+        <br />
+        <label>
+          ¿Todavía no tienes una cuenta?
+          <a href="" style="display: inline">Registrate</a>
+        </label>
+      </div>
     </div>
     <div class="rightDiv">
       <q-carousel
@@ -18,25 +87,37 @@
         transition-prev="slide-right"
         transition-next="slide-left"
         @mouseenter="autoplay = false"
-        @mouseleave="autoplay = 3500"
+        @mouseleave="autoplay = 5000"
         data-interval="10"
+        height="100vh"
       >
-        <q-carousel-slide
-          :name="1"
-          img-src="https://cdn.quasar.dev/img/mountains.jpg"
-        />
-        <q-carousel-slide
-          :name="2"
-          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
-        />
-        <q-carousel-slide
-          :name="3"
-          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
-        />
-        <q-carousel-slide
-          :name="4"
-          img-src="https://cdn.quasar.dev/img/quasar.jpg"
-        />
+        <template v-slot:navigation-icon="{ active, onClick }">
+          <q-btn
+            v-if="active"
+            size="xl"
+            style="padding: 0; margin: 0"
+            icon="remove"
+            color="white"
+            flat
+            round
+            dense
+            @click="onClick"
+          />
+          <q-btn
+            v-else
+            size="xs"
+            icon="fiber_manual_record"
+            color="white"
+            flat
+            round
+            dense
+            @click="onClick"
+          />
+        </template>
+        <q-carousel-slide :name="1" img-src="/login/lady-in-white.png" />
+        <q-carousel-slide :name="2" img-src="/login/doc.jpg" />
+        <q-carousel-slide :name="3" img-src="/login/nayib-amor.jpg" />
+        <q-carousel-slide :name="4" img-src="/login/consulta.jpg" />
       </q-carousel>
     </div>
   </div>
@@ -50,7 +131,7 @@ export default {
   data() {
     return {
       slide: 1,
-      autoplay: 3500,
+      autoplay: 5000,
     };
   },
   methods: {
@@ -97,12 +178,132 @@ export default {
 };
 </script>
 <style scoped>
+body {
+  background-color: #f8f9fb;
+}
 .rightDiv {
-  width: 60%;
+  width: 50%;
   display: inline-block;
+  height: 100vh;
 }
 .leftDiv {
-  width: 40%;
+  width: 50%;
+  height: 100vh;
   display: inline-block;
+  vertical-align: top;
+  text-align: center;
+}
+.login {
+  width: 100%;
+  height: 100vh;
+}
+.tabpanel div {
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+}
+h1 {
+  font-weight: bold;
+  margin: 0%;
+  margin-bottom: 2rem;
+  text-shadow: 0 0 80px rgb(0 0 0 / 23%);
+  color: #3c75ad;
+}
+.q-avatar {
+  height: 10rem;
+  width: 10rem;
+  margin-bottom: 4rem;
+}
+.horizontalCenter {
+  padding: 2rem;
+  position: relative;
+  top: 50%;
+  transform: translateY(-70%);
+}
+.q-btn {
+  margin: 1rem;
+}
+.top_header_left_side {
+  padding: 0rem;
+  padding-left: 1rem;
+  box-shadow: 0px -3px 20px 0px #1976d26e;
+}
+
+/* para hacer el wave header */
+.header {
+  position: relative;
+  text-align: center;
+  background: linear-gradient(to right, #1976d2a1, #c7ceff26);
+  color: white;
+}
+.inner-header {
+  height: 5vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.flex {
+  /*Flexbox for containers*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.waves {
+  position: relative;
+  width: 100%;
+  height: 5vh;
+  margin-bottom: -7px; /*Fix for safari gap*/
+  /* min-height: 100px;
+  max-height: 150px; */
+  /* transform: scale(0.5);
+  width: 100rem;
+  transform-origin: left; */
+}
+
+/* Animation  (waves)*/
+
+.parallax > use {
+  animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
+  0% {
+    transform: translate3d(-90px, 0, 0);
+  }
+  100% {
+    transform: translate3d(85px, 0, 0);
+  }
+}
+/* fin del wave header */
+/*Shrinking for mobile*/
+@media (max-width: 768px) {
+  .waves {
+    height: 40px;
+    min-height: 40px;
+  }
+  .content {
+    height: 30vh;
+  }
+  h1 {
+    font-size: 24px;
+  }
 }
 </style>
