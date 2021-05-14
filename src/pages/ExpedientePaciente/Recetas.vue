4bd23ffa19f548ab29ee485a914fefd53405093c
DanyAcuña <danyzyzy2000@gmail.com>
2021-05-14 01:30:27 -0600

    Cambios a las vistas de Recetas y Enfermedades Crónicas

--- src/pages/ExpedientePaciente/Recetas.vue
+++ src/pages/ExpedientePaciente/Recetas.vue
@@ -1,39 +1,46 @@
 <template>
   <div>
+    <h3 class="text-h3">Recetas</h3>
     <div class="q-pa-md q-gutter-sm">
-      <q-editor
-        v-model="editor"
+      <q-editor v-model="editor"
         :definitions="{
-          bold: { label: 'Style', icon: null, tip: 'style' },
+          save: {
+            tip: 'Guardar',
+            icon: 'save',
+            label: 'Guardar',
+            handler: saveWork,
+          },
         }"
+        :toolbar="[
+          ['bold', 'italic', 'strike', 'underline'],
+          ['upload', 'save'],
+        ]"
+        min-height="10rem"
       />
-      <div class="row justify-between">
-        <q-btn color="white" text-color="black" label="Guardar" />
-      </div>
     </div>
     <div class="row justify-between">
       <div class="col-md-5 q-mx-md">
-        <p class="text">Medicamentos Permanentes</p>
+        <p class="text-h6">Medicamentos Permanentes</p>
         <q-card
           class="my-card text-black"
           style="background: white(circle, #35a2ff 0%, #014a88 100%)"
         >
           <q-card-section>
-            <div class="text-h6">Fecha 03/05/2021</div>
-            <div class="text-subtitle2">Amoxicilina cada 8 horas</div>
             <div class="text-subtitle2">Fecha 03/05/2021</div>
+            <div class="text-subtitle2">Acetaminofén cada 8 horas</div>
+            <div class="text-subtitle2">Fecha 14/05/2021</div>
           </q-card-section>
         </q-card>
       </div>
       <div class="col-md-5 q-mx-md">
-        <p class="text">Historicos</p>
+        <p class="text-h6">Historicos</p>
         <q-card
           class="my-card text-black"
           style="background: white(circle, #35a2ff 0%, #014a88 100%)"
         >
           <q-card-section>
-            <div class="text-h6">Fecha 03/05/2021</div>
-            <div class="text-subtitle2">Paracetamol</div>
+            <div class="text-subtitle2">Fecha 03/05/2021</div>
+            <div class="text-subtitle2">Loratadina</div>
             <div class="text-subtitle2">Dosis:1 cada 8 horas</div>
           </q-card-section>
         </q-card>
@@ -49,11 +56,21 @@ export default {
       date: "2020/02/01",
       edad: "",
       genero: "",
-      editor: "",
+      editor: "<br> Medicamentos Permanentes:<br/>Acetaminofén </br>Loratadina<br/>" //texto html
     };
   },
   components: {
     BannerPrincipal,
+  },
+  methods: {
+    saveWork() {
+      this.$q.notify({
+        message: "Recetas guardadas en el local storage",
+        color: "green-4",
+        textColor: "white",
+        icon: "cloud_done",
+      });
+    },
   },
 };
 </script>
