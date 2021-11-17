8aba2e9e617800290f0b798f99144c5a4316cbed
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2021-11-16 21:27:08 -0600

    Update firebase.js
    Agregue analytics

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app"
 import "firebase/storage";
 import "firebase/firestore"
 import "firebase/auth"
+import "firebase/analytics"
 
 // Agregar configuración firebase:
 var firebaseConfig = {
@@ -11,7 +12,8 @@ var firebaseConfig = {
     projectId: "sistemamedico-b3d66",
     storageBucket: "sistemamedico-b3d66.appspot.com",
     messagingSenderId: "187184703055",
-    appId: "1:187184703055:web:56a0a19a52bd0e875e2c3a"
+    appId: "1:187184703055:web:56a0a19a52bd0e875e2c3a",
+    measurementId:"G-4H0R6DY511"
 }
 
 
@@ -19,5 +21,6 @@ let firebaseApp = firebase.initializeApp(firebaseConfig)
 let auth = firebase.auth()
 let db = firebase.firestore();
 let st = firebase.storage();
+let anal=firebase.analytics();
 
 export { firebase, auth, db, st }
